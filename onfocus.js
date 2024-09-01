83358293161b177ba46296d14b5ece7448dc432f
MAOSSE <110333261+MaoShiSanKe@users.noreply.github.com>
2024-09-01 16:10:27 +0800

    Update onfocus.js

--- onfocus.js
+++ onfocus.js
@@ -1,15 +1,14 @@
-// 浏览器切换标题 
-
+// 浏览器标题  快回来!!! 终于回来了
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://mssk.site/wp-content/uploads/2024/09/Niconiconi.webp");
+        $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = 'o(〃＾▽＾〃)o 你去哪了?!';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://mssk.site/wp-content/uploads/2024/09/Niconiconi.webp");
+        $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = '(✿◠‿◠) 你肥来啦!';
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
